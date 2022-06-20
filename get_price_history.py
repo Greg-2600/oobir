a72e27916b3f5675799c3c4990b53efb6d7c86d5
Greg Baker <gregbakerstl@gmail.com>
2022-06-20 17:11:25 -0500

    class price history, tests in main until py-test, organize src

--- get_price_history.py
+++
@@ -1,9 +0,0 @@
-import yahoo_fin.stock_info as si
-
-# get list of S&P 500 tickers
-tickers_sp = si.tickers_sp500()
-print(tickers_sp)
-
-# pull data for each S&P stock
-price_data = {ticker: si.get_data(ticker) for ticker in tickers_sp}
-print(price_data)
