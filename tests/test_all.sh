b0918106eef59c18539124b077f6d6ed7e416434
Greg Baker <gregbakerstl@gmail.com>
2022-06-23 15:01:58 -0500

    readme and tests

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+IP='localhost'
+curl "http://$IP:8000/fundamentals/ATT"
+curl "http://$IP:8000/fundamentals/TSLA"
+curl "http://$IP:8000/fundamentals/AMZN"
+curl "http://$IP:8000/price_history/GOOG"
+curl "http://$IP:8000/price_history/MSFT"
+curl "http://$IP:8000/price_history/CHTR"
+curl "http://$IP:8000/"
+curl "http://$IP:8000""
 
-curl "http://127.0.0.1:8000/fundamentals/ATT"
-curl "http://127.0.0.1:8000/fundamentals/TSLA"
-curl "http://127.0.0.1:8000/fundamentals/AMZN"
-curl http://127.0.0.1:8000/price_history/GOOG
-curl http://127.0.0.1:8000/price_history/MSFT
-curl http://127.0.0.1:8000/price_history/CHTR
-curl http://127.0.0.1:8000/
-curl http://127.0.0.1:8000/
 
-
-endpoint='http://127.0.0.1:8000';
+endpoint="http://$IP:8000";
 methods="fundamentals price_history"
 tickers="CHTR GOOG MSFT TSLA AMZN IBM"
 
 for ticker in $tickers; do
   for method in $methods; do
-    curl "$endpoint/$method/$ticker"
+    curl "$endpoint/$method/$ticker" &
   done
+  wait;
 done
 
+curl http://127.0.0.1:8000/price_history/ZBZZT
 curl http://127.0.0.1:8000/tickers
